3d907cc934e23896618c385677d814210c656a5c
karina <karyna.rein@gmail.com>
2023-01-25 16:39:06 +0300

    решена задача в папке 10

---
+++ 10/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int GetNumber()
+{
+    Console.WriteLine("Введите трехзначное число");
+    int number = Convert.ToInt32 (Console.ReadLine());
+    return number;
+}
+int number = GetNumber();
+int result = 0;
+result = (number / 10) % 10;
+Console.WriteLine(result);
